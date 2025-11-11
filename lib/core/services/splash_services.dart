828b7bde4eac499b903c0f70e0322b1eb24cfb42
Abdul Salam <abdulsalam.0302@gmail.com>
2025-11-11 12:35:26 +0500

    code pushed

--- lib/core/services/splash_services.dart
+++ lib/core/services/splash_services.dart
@@ -1,27 +1,27 @@
 import 'dart:async';
-// import 'package:flutter/material.dart';
 
 
-// import 'session_manager.dart';
+import '../../modules/home/presentation/barrel.dart';
+
 
 class SplashServices {
   void isLoggedIn() {
   Future.delayed(Duration(seconds: 2),(){
-    //  checkLoginStatus();
+     checkLoginStatus();
   });
   }
 
-  // Future<void> checkLoginStatus() async {
-  //   try {
-  //     await SessionController().getUserfromSharedpref();
+  Future<void> checkLoginStatus() async {
+    try {
+      await SessionController().getUserfromSharedpref();
 
-  //     if (SessionController().islogin == true) {
-  //       Get.offAllNamed(Routes.HOME);
-  //     } else {
-  //       Get.offAllNamed(Routes.LOGIN_SCREEN);
-  //     }
-  //   } catch (e) {
-  //     debugPrint('Error in checkLoginStatus: $e');
-  //   }
-  // }
+      if (SessionController().islogin == true) {
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        Get.offAllNamed(Routes.LOGIN_SCREEN);
+      }
+    } catch (e) {
+      debugPrint('Error in checkLoginStatus: $e');
+    }
+  }
 }
